e34619cddaf299956ee02840d4bd80dd0a18b6a9
Joe Atteen <joeyatteen@gmail.com>
2022-04-04 20:04:33 +0000

    Highlight nav link on scroll

--- js/script.js
+++ js/script.js
@@ -9,3 +9,55 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('black');
     }
 })
+
+// $(document).ready(function() {
+//     $('nav .links li a').click(function(event) {
+//         $('nav .links li a').removeClass('active');
+//         $(this).addClass('active');
+//         thisAttrHref = $(this).attr('href');
+//         thisAttrHrefOffset = $(thisAttrHref).offset().top + 50;
+//         $('html,body').animate({scrollTop:thisAttrHrefOffset});
+//         event.preventDefault();
+//     });
+// });
+
+// $window.scroll(function() {
+//     $('.container').each(function() {
+//         containerTop = $(this).offset().top;
+//         if($(document).scrollTop() > containerTop){
+//             thisOfs = $(this).attr('id');
+//             $('.container').removeClass('active');
+//             ActiveID =  $(this).addClass('active').attr('id');
+//         }
+//     });
+//     $('nav .links li').each(function(index, el) {
+//         thisChildren = $(this).children('a');
+//         thisChildrenHref = $(this).children('active').attr('href');
+//         if( thisChildrenHref === '#'+ActiveID){
+//             $('nav .links li a').removeClass('active');
+//             $(thisChildren).addClass('a');
+//         }
+//     });
+// });
+
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav .links li');
+const lg = document.querySelector('logo');
+window.addEventListener('scroll', () =>{
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= (sectionTop - sectionHeight / 2)){
+            current = section.getAttribute('id');
+        }
+    })
+    
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if (li.classList.contains(current)){
+            li.classList.add('active')
+        }
+    })
+
+})
