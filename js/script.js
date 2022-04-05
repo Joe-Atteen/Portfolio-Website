c8674eec3e8d842d9f517b37e0e625ee9463307f
Joe Atteen <joeyatteen@gmail.com>
2022-04-05 14:24:21 +0000

    Meaningful text addition

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+//background color on navbar on scroll
 const nav = document.querySelector('nav');
-
 window.addEventListener('scroll', () => {
     console.log(window.scrollY);
     if (window.scrollY >= 50) {
@@ -10,36 +10,7 @@ window.addEventListener('scroll', () => {
     }
 })
 
-// $(document).ready(function() {
-//     $('nav .links li a').click(function(event) {
-//         $('nav .links li a').removeClass('active');
-//         $(this).addClass('active');
-//         thisAttrHref = $(this).attr('href');
-//         thisAttrHrefOffset = $(thisAttrHref).offset().top + 50;
-//         $('html,body').animate({scrollTop:thisAttrHrefOffset});
-//         event.preventDefault();
-//     });
-// });
-
-// $window.scroll(function() {
-//     $('.container').each(function() {
-//         containerTop = $(this).offset().top;
-//         if($(document).scrollTop() > containerTop){
-//             thisOfs = $(this).attr('id');
-//             $('.container').removeClass('active');
-//             ActiveID =  $(this).addClass('active').attr('id');
-//         }
-//     });
-//     $('nav .links li').each(function(index, el) {
-//         thisChildren = $(this).children('a');
-//         thisChildrenHref = $(this).children('active').attr('href');
-//         if( thisChildrenHref === '#'+ActiveID){
-//             $('nav .links li a').removeClass('active');
-//             $(thisChildren).addClass('a');
-//         }
-//     });
-// });
-
+//nav link highlight on scroll
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav .links li');
 const lg = document.querySelector('logo');
